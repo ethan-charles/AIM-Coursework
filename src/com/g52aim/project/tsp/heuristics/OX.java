d9412e1f31782740618780ed60afda52258e7881
Vincent Chee Jia Hong <cheehong05090@gmail.com>
2019-04-21 09:08:56 +0100

    fix array out of bound and make the cutpoint inclusive

--- src/com/g52aim/project/tsp/heuristics/OX.java
+++ src/com/g52aim/project/tsp/heuristics/OX.java
@@ -32,10 +32,10 @@ public class OX extends CrossoverHeuristicOperators implements XOHeuristicInterf
         int times = getIncrementalTimes(intensityOfMutation);
         int[][] childTuple;
         for (int i = 0; i < times; i++) {
-            int[] child1 = p1Array.clone();
-            int[] child2 = p1Array.clone();
+            int[] p1clone = p1Array.clone();
+            int[] p2clone = p1Array.clone();
             // pass value not reference
-            childTuple = ActualOX(child1, child2);
+            childTuple = ActualOX(p1clone, p2clone);
             // reassigning reference
             p1Array = childTuple[0];
             p2Array = childTuple[1];
@@ -66,48 +66,54 @@ public class OX extends CrossoverHeuristicOperators implements XOHeuristicInterf
             cutpoint_2 = numberOfPointsWillStay;
         }
 
+
         numberOfPointsWillStay = cutpoint_2 - cutpoint_1;
+        // skip the process when cutpoint1 and cutpoint2 is first and last indices
+        if (numberOfPointsWillStay == n - 1) {
+            return new int[][]{p2Array, p1Array};
+        }
+
         int[] locationList_p1 = listLocationOfElement(p1Array);
         int[] locationList_p2 = listLocationOfElement(p2Array);
 
         int ele;
         int pointOfInsertion;
-        int[] some1 = p1Array.clone();
-        int[] some2 = p2Array.clone();
-        // "nullify" the location index of staying element, so they will not get considered later
-        // so there's no duplicate when copy from one to another
-        for (pointOfInsertion = 0; pointOfInsertion < numberOfPointsWillStay; pointOfInsertion++) {
-            int locationOfEle;
-
-            // get the element starting from the cutoff point
-            ele = p1Array[cutpoint_1 + pointOfInsertion];
-            // get the location of the element in p2
-            System.out.println("location: " + ele);
-            locationOfEle = locationList_p2[ele];
-            // set the ele to -1 so will not get copied later
-            some2[locationOfEle] = -1;
-
-            // do the same for child1
-            ele = p2Array[cutpoint_1 + pointOfInsertion];
-            locationOfEle = locationList_p1[ele];
-            some1[locationOfEle] = -1;
-        }
-        int[] a = new int[n];
-//         copy to a temp array to rearrange structure
-//        right shifted
-        System.arraycopy(some1, cutpoint_2 + 1, a, 0, n - cutpoint_2 - 1);
-        System.arraycopy(some1, 0, a, n - cutpoint_2 - 1, cutpoint_2 + 1);
-        int[] b = new int[n];
-        System.arraycopy(some2, cutpoint_2 + 1, b, 0, n - cutpoint_2 - 1);
-        System.arraycopy(some2, 0, b, n - cutpoint_2 - 1, cutpoint_2 + 1);
-
+//        act as temp parent
         int[] child1 = p1Array.clone();
         int[] child2 = p2Array.clone();
+        // "nullify" the location index of staying element, so they will not get considered later
+        // so there's no duplicate when copy from one to another
+        for (pointOfInsertion = 0; pointOfInsertion <= numberOfPointsWillStay; pointOfInsertion++) {
+            int locationOfEle;
+            // get the element starting from the cutoff point
+            ele = child1[cutpoint_1 + pointOfInsertion];
+            // get the location of the element in p2
+            // find location using city id - 1
+            locationOfEle = locationList_p2[ele - 1];
+            // set the ele to -1 so will not get copied later
+            p2Array[locationOfEle] = -1;
+
+            // do the same for child1
+            ele = child2[cutpoint_1 + pointOfInsertion];
+            locationOfEle = locationList_p1[ele - 1];
+            p1Array[locationOfEle] = -1;
+        }
+//         copy to a temp array to rearrange structure
+//        be inclusive
+//        handle the case when cutpoint is at border
+        int[] a = new int[n];
+        System.arraycopy(p1Array, cutpoint_2 + 1, a, 0, (n - 1) - cutpoint_2);
+        System.arraycopy(p1Array, 0, a, (n - 1) - cutpoint_2, cutpoint_2 + 1);
+
+        int[] b = new int[n];
+        System.arraycopy(p1Array, cutpoint_2 + 1, b, 0, (n - 1) - cutpoint_2);
+        System.arraycopy(p1Array, 0, b, (n - 1) - cutpoint_2, cutpoint_2 + 1);
+
 
         // copy from p1Copy to child2
 //        dont copy if some1[counter] is -1, else retrieve value a[counter]
         int counter = 0;
-        for (int i = 0; i <= cutpoint_1; i++) {
+        for (int i = 0; i < cutpoint_1; i++) {
             ele = a[counter];
             if (ele != -1) {
                 child2[i] = ele;
@@ -126,11 +132,9 @@ public class OX extends CrossoverHeuristicOperators implements XOHeuristicInterf
             counter++;
         }
 
-
-
         // now copy from p2Copy to child1
         counter = 0;
-        for (int i = 0; i <= cutpoint_1; i++) {
+        for (int i = 0; i < cutpoint_1; i++) {
             ele = b[counter];
             if (ele != -1) {
                 child1[i] = ele;
@@ -139,7 +143,7 @@ public class OX extends CrossoverHeuristicOperators implements XOHeuristicInterf
             }
             counter++;
         }
-        for (int i = cutpoint_2+1; i < n; i++) {
+        for (int i = cutpoint_2 + 1; i < n; i++) {
             ele = b[counter];
             if (ele != -1) {
                 child1[i] = ele;
