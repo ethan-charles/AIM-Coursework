a63854ff0828b3a46214f30a8764aba22a1e37e8
Vincent Chee Jia Hong <cheehong05090@gmail.com>
2019-04-26 10:39:12 +0100

    revert

--- src/com/g52aim/project/tsp/runners/SR_IE_Runner.java
+++ src/com/g52aim/project/tsp/runners/SR_IE_Runner.java
@@ -7,19 +7,19 @@ import AbstractClasses.HyperHeuristic;
 import AbstractClasses.ProblemDomain;
 
 public class SR_IE_Runner {
-	
-	public static void main(String [] args) {
-		
-		long seed = 15032018l;
-		long timeLimit = 1_000l;
-		ProblemDomain problem = new G52AIMTSP(seed);
-		problem.loadInstance(2);
-		HyperHeuristic hh = new SR_IE_HH(seed);
-		hh.setTimeLimit(timeLimit);
-		hh.loadProblemDomain(problem);
-		hh.run();
-		
-		System.out.println("f(s_best) = " + hh.getBestSolutionValue());
-	}
+
+    public static void main(String[] args) {
+
+        long seed = 15032018l;
+        long timeLimit = 1_000l;
+        ProblemDomain problem = new G52AIMTSP(seed);
+        problem.loadInstance(2);
+        HyperHeuristic hh = new SR_IE_HH(seed);
+        hh.setTimeLimit(timeLimit);
+        hh.loadProblemDomain(problem);
+        hh.run();
+
+        System.out.println("f(s_best) = " + hh.getBestSolutionValue());
+    }
 
 }
